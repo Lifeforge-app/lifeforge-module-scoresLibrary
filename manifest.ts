26920c65603f1db0df13a9760eb52fc2071ae0cd
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 12:01:59 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,10 +2,7 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Scores Library',
-  icon: 'tabler:file-music',
   routes: {
     '/': lazy(() => import('@'))
   },
-  category: 'Storage'
 } satisfies ModuleConfig
